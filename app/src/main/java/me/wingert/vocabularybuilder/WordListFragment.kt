df8c9de3675f1cb65557687fdf1f7a9ca31aa406
keekah <kika.wingert@gmail.com>
2021-02-07 10:58:06 -0800

    clean up

--- app/src/main/java/me/wingert/vocabularybuilder/WordListFragment.kt
+++ app/src/main/java/me/wingert/vocabularybuilder/WordListFragment.kt
@@ -1,13 +1,9 @@
 package me.wingert.vocabularybuilder
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.LinearLayout
-import android.widget.ScrollView
-import android.widget.TextView
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
@@ -19,21 +15,35 @@ class WordListFragment : Fragment() {
     private lateinit var binding : FragmentWordListBinding
     private lateinit var viewModel : WordListViewModel
     private lateinit var viewModelFactory : WordListViewModelFactory
+    private lateinit var adapter : WordAdapter
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
 
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_word_list, container, false)
 
+        initializeViewModel()
 
+        initializeAdapter()
+
+        setAddButtonClickListener()
+
+        return binding.root
+    }
+
+    private fun initializeViewModel() {
         viewModelFactory = WordListViewModelFactory()
         viewModel = ViewModelProvider(this, viewModelFactory).get(WordListViewModel::class.java)
 
         binding.viewModel = viewModel
+    }
 
+    private fun initializeAdapter() {
+        adapter = WordAdapter()
 
-        val adapter = WordAdapter()
         binding.wordList.adapter = adapter
+    }
 
+    private fun setAddButtonClickListener() {
         binding.addButton.setOnClickListener { viewModel.onAdd(binding.wordEdit.text) }
 
         viewModel.wordList.observe(viewLifecycleOwner, Observer {
@@ -41,8 +51,6 @@ class WordListFragment : Fragment() {
                 adapter.data = it
             }
         })
-
-        return binding.root
     }
 
 }
