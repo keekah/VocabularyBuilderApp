394b3ddcc86d4cb2ce8a1b91c3795b3667658d2e
keekah <kika.wingert@gmail.com>
2021-02-10 22:26:25 -0800

    add image to view holder

--- app/src/main/java/me/wingert/vocabularybuilder/WordAdapter.kt
+++ app/src/main/java/me/wingert/vocabularybuilder/WordAdapter.kt
@@ -2,12 +2,14 @@ package me.wingert.vocabularybuilder
 
 import android.util.Log
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import me.wingert.vocabularybuilder.database.VocabularyWord
 
-class WordAdapter : RecyclerView.Adapter<WordAdapter.WordViewHolder>() {
+class WordAdapter : RecyclerView.Adapter<WordAdapter.ViewHolder>() {
 
     var data = listOf<VocabularyWord>()
         set(value) {
@@ -19,16 +21,34 @@ class WordAdapter : RecyclerView.Adapter<WordAdapter.WordViewHolder>() {
         return data.size
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): WordViewHolder {
-        val layoutInflater = LayoutInflater.from(parent.context)
-        val view = layoutInflater.inflate(R.layout.word_view, parent, false) as TextView
-        return WordViewHolder(view)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder.from(parent)
     }
 
-    override fun onBindViewHolder(holder: WordViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val item = data[position]
-        holder.textView.text = item.word
+        holder.bind(item)
     }
 
-    class WordViewHolder(val textView: TextView) : RecyclerView.ViewHolder(textView)
+    class ViewHolder private constructor(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+        val wordText : TextView = itemView.findViewById(R.id.word_text)
+        val deleteIcon : ImageView = itemView.findViewById(R.id.delete_icon)
+
+        companion object {
+
+            fun from(parent: ViewGroup) : ViewHolder {
+                val layoutInflater = LayoutInflater.from(parent.context)
+                val view = layoutInflater.inflate(R.layout.word_view, parent, false)
+                return ViewHolder(view)
+            }
+        }
+
+        fun bind(item: VocabularyWord) {
+            val res = itemView.context.resources
+
+            wordText.text = item.word
+//            deleteIcon.setImageResource(R.drawable.ic_baseline_delete_24)
+        }
+    }
 }
