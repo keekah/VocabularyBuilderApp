9fa5337445f10ce51549ab7b5635f547ed663735
keekah <kika.wingert@gmail.com>
2021-02-17 09:45:05 -0800

    add navigation drawer

--- app/src/main/java/me/wingert/vocabularybuilder/MainActivity.kt
+++ app/src/main/java/me/wingert/vocabularybuilder/MainActivity.kt
@@ -2,10 +2,42 @@ package me.wingert.vocabularybuilder
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupWithNavController
+import kotlinx.android.synthetic.main.activity_main.*
+import me.wingert.vocabularybuilder.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var navController : NavController
+    private lateinit var drawerLayout : DrawerLayout
+    private lateinit var navHostFragment : NavHostFragment
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+
+        navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+        drawerLayout = binding.drawerLayout
+
+        appBarConfiguration = AppBarConfiguration(navController.graph, drawerLayout)
+
+        NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout)
+        binding.navView.setupWithNavController(navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
 }
